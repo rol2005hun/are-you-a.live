50ffa0a369e44bb0e0533ad909f9067daef759e4
Ranyák Roland Adalbert <81093097+rol2005hun@users.noreply.github.com>
2023-11-02 19:39:20 +0100

    Add files via upload

---
+++ client/pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="alive-container">
+        <p class="alive-text">{{ aliveText }}</p>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const aliveText = ref('If you see this, probably you\'r alive.');
+
+const aliveTextWithAnimation = computed(() => {
+    const text = aliveText.value.split(' ');
+    let result = '';
+    for (let i = 0; i < text.length; i++) {
+        result += text[i].split('').reduce((acc, cur) => {
+            acc += cur;
+            return acc + ' -> ' + acc;
+        }, '');
+        result += ' ';
+    }
+    return result.trim();
+});
+</script>
+
+<style lang="scss">
+@import '@/assets/scss/index.scss';
+</style>
