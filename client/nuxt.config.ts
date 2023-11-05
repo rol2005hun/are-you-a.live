9f0e4ee8f9d8d28e1cce3a5b37bc93483c707ac9
Ranyák Roland Adalbert <81093097+rol2005hun@users.noreply.github.com>
2023-11-05 14:53:45 +0100

    Add files via upload

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,4 +1,25 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+import { NuxtConfig } from '@nuxt/types';
+
+const config: NuxtConfig = {
+  devtools: { enabled: true },
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'hu'
+      },
+      title: 'Are you alive?',
+      link: [
+        { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css', crossorigin: 'anonymous' },
+        { rel: 'icon', href: 'https://i.imgur.com/uYWgYwg.png', type: 'image/x-icon' }
+      ],
+      meta: [
+        { name: 'title', content: 'Wolimby - Social' },
+        { name: 'description', content: 'eZAZ.' }
+      ]
+    },
+    rootId: 'app'
+  },
+  buildModules: ['@pinia/nuxt'],
+}
+
+export default config;
